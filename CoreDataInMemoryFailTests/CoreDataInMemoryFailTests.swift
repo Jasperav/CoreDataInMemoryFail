6ca5603dac8e0d57b61ed7712677d1b1adcdb97e
Subramanian Mariappan <subramanian.mariappan@zohocorp.com>
2019-10-31 13:09:26 +0530

    Test updated such that persistentStore will be migrated to type NSInMemoryStoreType

--- CoreDataInMemoryFailTests/CoreDataInMemoryFailTests.swift
+++ CoreDataInMemoryFailTests/CoreDataInMemoryFailTests.swift
@@ -5,14 +5,12 @@ import XCTest
 
 class CoreDataInMemoryFailTests: XCTestCase {
 
-    private func createContainer(modify: (NSPersistentStoreDescription) -> ()) -> NSPersistentContainer {
+    private func createContainer(modify: (NSPersistentContainer) -> ()) -> NSPersistentContainer {
         let bundle = Bundle(for: type(of: self))
         let path = bundle.path(forResource: "InMemoryDatabase", ofType: "sqlite")!
         let url = URL(fileURLWithPath: path)
         let persistentContainer = createPersistentContainer(dataModelName: "InMemoryDatabase")
         let storeDescription = NSPersistentStoreDescription(url: url)
-
-        modify(storeDescription)
 
         persistentContainer.persistentStoreDescriptions = [storeDescription]
         persistentContainer.loadPersistentStores { description, error in
@@ -20,13 +18,31 @@ class CoreDataInMemoryFailTests: XCTestCase {
             XCTAssertNil(error)
         }
 
+        modify(persistentContainer)
         return persistentContainer
     }
 
     func testFail() {
         let persistentContainer = createContainer(modify: { _ in })
-        let inMemoryContainer = createContainer { storeDescription in
-            storeDescription.type = NSInMemoryStoreType
+        let inMemoryContainer = createContainer { persistentContainer in
+            let coordinator = persistentContainer.persistentStoreCoordinator
+            coordinator.persistentStores.forEach { (persistentStore) in
+                do {
+                    try coordinator.migratePersistentStore(persistentStore, to: NSPersistentContainer.defaultDirectoryURL(), options: nil, withType: NSInMemoryStoreType)
+                } catch {
+                    print("Error while migrating persistentStore")
+                }
+            }
+        }
+
+        let persistentContainerCoordinator = persistentContainer.persistentStoreCoordinator
+        persistentContainerCoordinator.persistentStores.forEach { (persistentStore) in
+            XCTAssertEqual(persistentStore.type, "SQLite")
+        }
+
+        let inMemoryContainerCoordinator = inMemoryContainer.persistentStoreCoordinator
+        inMemoryContainerCoordinator.persistentStores.forEach { (persistentStore) in
+            XCTAssertEqual(persistentStore.type, NSInMemoryStoreType)
         }
 
         let fetchRequest: NSFetchRequest<Person> = Person.fetchRequest()
